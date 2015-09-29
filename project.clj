ca4658ff12751fc66f5dcbbaced2a0b6fc552328
roman_lubushkin <ra.lubushkin@gamil.com>
2015-09-29 09:39:32 +0800

    Update to Bundle 7.7.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject export-server "1.1.0"
-  :description "AnyChart export server"
+(defproject export-server "1.1.1"
+  :description "AnyChart export server, AnyChart Bundle version 7.7.0"
   :url "https://github.com/AnyChart/export-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
