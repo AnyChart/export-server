1a310155de7040c33f1557c084926c6fba1f03f1
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-10-02 12:30:01 +0800

    Update anychart-bundle due to release 8.0.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject export-server "1.5.1"
-  :description "AnyChart export server, AnyChart Bundle version 7.14.0"
+(defproject export-server "1.5.2"
+  :description "AnyChart export server, AnyChart Bundle version 8.0.0"
   :url "https://github.com/AnyChart/export-server"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
